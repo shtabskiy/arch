4cd08f7c3becf85bb23ff4eee67d1a423733d1b1
shtabskiy <41116895+shtabskiy@users.noreply.github.com>
2018-07-15 23:13:17 +0300

    Update arch_install_with_graphics.sh

--- arch_install_with_graphics.sh
+++ arch_install_with_graphics.sh
@@ -29,7 +29,7 @@ genfstab -pU /mnt >> /mnt/etc/fstab
 # Keyboard, locale, time
 arch-chroot /mnt useradd -m -g users -G audio,games,lp,optical,power,scanner,storage,video,wheel -s /bin/bash ads
 arch-chroot /mnt passwd ads
-arch-chroot /mnt pacman -S xorg-server xorg-xinit xorg-apps mesa-libgl xterm xf86-video-vesa cinnamon file-roller gvfs-smb samba cifs-utils sddm firefox mc chromium alsa-utils  gnome-terminal gthumb vlc audacious gedit htop screenfetch --noconfirm
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xorg-apps mesa-libgl xterm xf86-video-vesa cinnamon arc-icon-theme arc-gtk-theme file-roller gvfs-smb samba cifs-utils sddm firefox mc chromium alsa-utils  gnome-terminal gthumb vlc audacious gedit htop screenfetch --noconfirm
 arch-chroot /mnt systemctl enable sddm.service
 arch-chroot /mnt systemctl enable NetworkManager.service
 arch-chroot /mnt /bin/bash -c '
